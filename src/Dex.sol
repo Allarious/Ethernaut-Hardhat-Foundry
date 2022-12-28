9c05ac39e974384bbfc4abfbd64c81a4af7e35a5
allarious <arjmands@ualberta.ca>
2022-12-28 13:30:57 -0700

    updated imports

--- src/Dex.sol
+++ src/Dex.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "openzeppelin-contracts-08/token/ERC20/IERC20.sol";
-import "openzeppelin-contracts-08/token/ERC20/ERC20.sol";
-import 'openzeppelin-contracts-08/access/Ownable.sol';
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import 'openzeppelin-contracts/access/Ownable.sol';
 
 contract Dex is Ownable {
   address public token1;
