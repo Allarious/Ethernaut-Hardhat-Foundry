78bc522573e4f3ea55d2ecaa623575e8bc6be449
allarious <arjmands@ualberta.ca>
2023-03-04 23:47:32 -0700

    test file

--- test/TestContract.t.sol
+++ test/TestContract.t.sol
@@ -4,19 +4,18 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 import "../src/TestContract.sol";
-import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract TestContractTest is Test {
     TestContract t;
 
     function setUp() public {
-        t = new TestContract();
     }
 
-    function testEncodePacked() public {
-        address _address = address(1);
-        console.log(address(1));
-        console.logBytes(abi.encodePacked("10", _address));
-        assertEq(abi.encodePacked("", _address), abi.encodePacked("", ERC20(_address)));
+    function testAddressZero() public {
+        (bool success, bytes memory returndata) = address(0).staticcall(
+            abi.encodeWithSignature("getData()")
+        );
+        console.logBytes(returndata);
+        console.log(returndata.length);
     }
 }
